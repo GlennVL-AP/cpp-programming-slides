f8a38699882cec8fca94d024622b81eaab64510b
Glenn Van Loon (p151886) <glenn.vanloon@ap.be>
2025-03-29 17:57:43 +0100

    add reveal.js timer plugin

--- server.js
+++ server.js
@@ -135,6 +135,7 @@ app.get('/', (req, res, next) => {
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/reveal.js", express.static(path.join(__dirname, "node_modules", "reveal.js")));
 app.use("/reveal.js-mermaid-plugin", express.static(path.join(__dirname, "node_modules", "reveal.js-mermaid-plugin")));
+app.use("/reveal.js-countdown", express.static(path.join(__dirname, "node_modules", "reveal.js-countdown")));
 
 app.use((req, res, next) => {
     res.status(404).render("error", {
